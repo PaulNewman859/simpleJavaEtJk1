98ad280741afa1ad1eeee8b2b30b6ef0fe7a03a8
PaulNewman859 <60698068+PaulNewman859@users.noreply.github.com>
2024-07-20 17:52:14 +0200

    Update Main.java za direct web

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!  v02 br1");
+        System.out.println("Hello world!  v0411 br1");
     }
 }
